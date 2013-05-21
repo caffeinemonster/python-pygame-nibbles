894c265559c5ab35896ee6d20543cdf7e3219de8
caffeinemonster <illskills1982@googlemail.com>
2013-05-21 01:12:37 +0100

    Less is more.

--- pgnibbles-class.py
+++ pgnibbles-class.py
@@ -10,6 +10,7 @@ class csnake():
     def __init__(self):
 	direction = 0
 	self.data = []
+	self.alive = 1
     def add (self, x, y):
 	self.data.insert(0,(x,y))
     def head(self):
@@ -30,7 +31,6 @@ class capple():
 	r = self.data
 	return r
 while (1):
-    global GCLOCK, GSURF, BASICFONT
     pygame.init()
     GCLOCK = pygame.time.Clock()
     GSURF = pygame.display.set_mode((wWIDTH, wHEIGHT))
@@ -41,17 +41,11 @@ while (1):
     sy = random.randint(5, GHEIGHT - 6)
     snake.add(sx,sy)
     snake.add(sx-1,sy)
-    #snake.add(sx-2,sy)
-    #snake.add(sx-3,sy)
     apple.add(random.randint(0, GWIDTH - 1),random.randint(0, GHEIGHT - 1))
-    while(1): #start game
-        print snake.direction
+    while(1):
 	for event in pygame.event.get():
             if event.type == QUIT:
         	quit()    
-	#    stop()
-	    
-	#print "keydown"
             elif event.type == KEYDOWN:
                 if ((event.key == K_LEFT) and snake.direction != K_RIGHT):
                     snake.direction = K_LEFT
@@ -64,25 +58,14 @@ while (1):
                 if event.key == K_ESCAPE:
                     quit()
         if snake.data[0][0] == -1 or snake.data[0][0] == GWIDTH or snake.data[0][1] == -1 or snake.data[0][1] == GHEIGHT:
-            break
-        sbody = snake.body()
-	shead = snake.head()
-	#del sbody[0]
-        #ishead = 1
-	print sbody
-	print shead
-	print "========================"
-	for body in sbody[1:]:
-	    print body
-	    print shead
-	    print "==========="
-	    if (body == shead):
-	        quit()
+            snake.alive = 0
+	for body in snake.data[1:]:
+	    if (body == snake.head()):
+	        snake.alive = 0
 	if snake.data[0][0] == apple.data[0][0] and snake.data[0][1] == apple.data[0][1]:
             apple.add(random.randint(0, GWIDTH - 1),random.randint(0, GHEIGHT - 1))
         else:
 	    snake.remove()
-
 	if snake.direction == K_UP:
             snake.add(snake.data[0][0], snake.data[0][1] - 1)
         elif snake.direction == K_DOWN:
@@ -92,9 +75,10 @@ while (1):
         elif snake.direction == K_RIGHT:
             snake.add(snake.data[0][0] + 1, snake.data[0][1])
         GSURF.fill((0,0,0))
-        sdata = snake.data
-	for co in sdata:
+	for co in snake.data:
             pygame.draw.circle(GSURF, (0, 255, 0), (co[0]*GSIZE+(GSIZE/2),co[1]*GSIZE+(GSIZE/2)), GSIZE/2, 2)
         pygame.draw.circle(GSURF, (255, 0, 0), (apple.data[0][0]*GSIZE+(GSIZE/2),apple.data[0][1]*GSIZE+(GSIZE/2)), GSIZE/2, 2)
         pygame.display.update()
         GCLOCK.tick(15)
+	if snake.alive==0:
+	    break
