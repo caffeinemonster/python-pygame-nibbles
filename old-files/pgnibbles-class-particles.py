1afec8c07ca415ad3583e00771aa923deb5098d6
tea <tea@crunchbang.(none)>
2013-05-22 23:34:12 +0100

    Tidying + sounds.

--- old-files/pgnibbles-class-particles.py
+++ old-files/pgnibbles-class-particles.py
@@ -9,7 +9,7 @@ GWIDTH = int(wWIDTH / GSIZE)
 GHEIGHT = int(wHEIGHT / GSIZE)
 
 
-class csnake():  # snake class
+class csnake():
     def __init__(self):
         self.direction = 0
         self.data = []
@@ -30,7 +30,7 @@ class csnake():  # snake class
         self.data.pop()
 
 
-class capple():  # apple class
+class capple():
     def __init__(self):
         self.data = []
 
@@ -39,7 +39,7 @@ class capple():  # apple class
         self.data.append((x, y))
 
 
-class cscore():  # score class
+class cscore():
     def __init__(self):
         self.score = 0
 
@@ -47,7 +47,7 @@ class cscore():  # score class
         self.score = self.score + s
 
 
-class cparticle():  # particle class
+class cparticle():
     def __init__(self):
         self.size = 0
         self.x = 0
@@ -87,7 +87,7 @@ class cparticle():  # particle class
             self.alive = 0
 
 
-class cparticles():  # particle group class
+class cparticles():
     def __init__(self):
         self.particles = []
 
@@ -107,59 +107,36 @@ class cparticles():  # particle group class
             if p.alive != 1:
                 del(p)
 
-
-class csounds():  # sound effects class
-    def __init__(self):
-        #super(csounds, self).__init__()
-        self.sndeat = pygame.mixer.Sound('beep.wav')
-        self.snddead = pygame.mixer.Sound('dead.wav')
-
-    def fxdead(self):
-        self.snddead.play()
-
-    def fxeat(self):
-        self.sndeat.play()
-
-
-def terminate():  # quit function
-    pygame.mixer.quit()
-    pygame.quit()
-    quit()
-
-
-def main():  # main game loop
-    r = 1
+while (1):
     pygame.init()
     GCLOCK = pygame.time.Clock()
     GSURF = pygame.display.set_mode((wWIDTH, wHEIGHT))
     pygame.display.set_caption("pgnibbles")
-    sfx = csounds()
     apple = capple()
     snake = csnake()
     snake.direction = K_RIGHT
     sx = random.randint(5, GWIDTH - 6)
     sy = random.randint(5, GHEIGHT - 6)
-    snake.add(sx - 2, sy)
-    snake.add(sx - 1, sy)
     snake.add(sx, sy)
+    snake.add(sx - 1, sy)
     score = cscore()
     particles = cparticles()
     apple.add(random.randint(0, GWIDTH - 1), random.randint(0, GHEIGHT - 1))
     while(1):
         for event in pygame.event.get():
             if event.type == QUIT:
-                r = 0
+                quit()
             elif event.type == KEYDOWN:
-                if event.key == K_LEFT and snake.direction != K_RIGHT:
+                if ((event.key == K_LEFT) and snake.direction != K_RIGHT):
                     snake.direction = K_LEFT
-                if event.key == K_RIGHT and snake.direction != K_LEFT:
+                if (event.key == K_RIGHT) and snake.direction != K_LEFT:
                     snake.direction = K_RIGHT
-                if event.key == K_UP and snake.direction != K_DOWN:
+                if (event.key == K_UP) and snake.direction != K_DOWN:
                     snake.direction = K_UP
-                if event.key == K_DOWN and snake.direction != K_UP:
+                if (event.key == K_DOWN) and snake.direction != K_UP:
                     snake.direction = K_DOWN
                 if event.key == K_ESCAPE:
-                    r = 0
+                    quit()
         if snake.data[0][0] == -1 or snake.data[0][0] == GWIDTH or snake.data[0][1] == -1 or snake.data[0][1] == GHEIGHT:
             snake.alive = 0
         for body in snake.data[1:]:
@@ -171,7 +148,6 @@ def main():  # main game loop
             particles.seed(GSURF.get_width() - 20, 475, 30, 15, 100)
             apple.add(random.randint(0, GWIDTH - 1), random.randint(0, GHEIGHT - 1))
             score.add(1)
-            sfx.fxeat()
         else:
             snake.remove()
         if snake.direction == K_UP:
@@ -192,7 +168,7 @@ def main():  # main game loop
         pygame.draw.circle(GSURF, (255, 0, 0), (apple.data[0][0]*GSIZE+(GSIZE/2),apple.data[0][1]*GSIZE+(GSIZE/2)), GSIZE/2, 2)
         if pygame.font:
             font = pygame.font.Font(None, 24)
-            text = font.render("Score : " + str(score.score), 1, (255, 255, 255))
+            text = font.render("Score:" + str(score.score), 1, (255, 255, 255))
             textpos = text.get_rect(centerx=GSURF.get_width() / 2)
             GSURF.blit(text, textpos)
             text = font.render("pgnibbles", 1, (255, 255, 255))
@@ -203,23 +179,4 @@ def main():  # main game loop
         if snake.alive == 0:
             if (score.score != 0):
                 print(("You died your score was " + str(score.score)))
-            sfx.fxdead()
-            return r
             break
-
-        if r != 1:
-            return r
-
-if __name__ == "__main__":
-    r = 1
-    pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
-    ##pygame.mixer.music.set_endevent(SONG_END)
-    pygame.mixer.music.load('music/TRACK01.mp3')
-    #pygame.mixer.music.load('music/TRACK02.mp3')
-    #pygame.mixer.music.queue('music/TRACK03.mp3')
-    #pygame.mixer.music.queue('music/TRACK04.mp3')
-    #pygame.mixer.music.queue('music/TRACK05.mp3')
-    pygame.mixer.music.play(-1)
-    #pygame.display.toggle_fullscreen()
-    while r != 0:  # quit if return not equal to 0
-        r = main()
